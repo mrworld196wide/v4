ec51898113b1be9b1a8eccc3310b01254bb66ebf
mrworld196wide <shubhum19@gmail.com>
2023-08-23 02:08:37 +0530

    once again

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -64,9 +64,10 @@ const Hero = () => {
   const four = (
     <p>
       I'm a senior year student from NIIT University with a 7.56 CGPA and a passionate software
-      developer with a goal-driven creative mindset and passion for learning and innovating. I love
-      building things for the web. Outside work, I occasionally blog on{' '}
-      <a href="https://medium.com/@apkc4545">Medium</a>.
+      developer with a goal-driven creative mindset and passion for learning and innovating. And I
+      love building things for the web.
+      <br />
+      Outside work, I occasionally blog on <a href="https://medium.com/@apkc4545">Medium</a>.
     </p>
   );
   const five = (
